78c39448109c04c3f9e679a6891800db85bf0113
nlizcx <169065802+nlizcx@users.noreply.github.com>
2024-06-17 09:34:55 -0600

    ygyugtgyu

---
+++ Cronometro/Main.gd
@@ -0,0 +1,17 @@
+extends CanvasLayer
+
+var minutes = 1
+var seconds = 0
+var is_running = false
+
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_start_stop_button_pressed():
+	is_running
